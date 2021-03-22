4f58a238301d31ea70d770dbd4796c8edcb770a3
hexia <812866087@qq.com>
2021-03-22 16:17:16 +0800

    同步代码

--- app/src/main/java/com/example/skindemo/MyApplication.java
+++ app/src/main/java/com/example/skindemo/MyApplication.java
@@ -2,10 +2,13 @@ package com.example.skindemo;
 
 import android.app.Application;
 
+import com.example.skindemo.skin.SkinConfigManager;
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        SkinConfigManager.getInstance().init(this);
     }
 }
--- app/src/main/java/com/example/skindemo/skin/SkinConfigManager.java
+++ app/src/main/java/com/example/skindemo/skin/SkinConfigManager.java
@@ -1,12 +1,36 @@
 package com.example.skindemo.skin;
 
-import android.content.res.Resources;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.File;
+
 public class SkinConfigManager {
 
+    /**
+     * 夜间模式
+     */
+    private static final String SKIN_MODE_NIGHT = "night";
+    /**
+     * 日间模式
+     */
+    private static final String SKIN_MODE_LIGHT = "light";
+    /**
+     * 当前皮肤模式
+     */
+    private static final String KEY_CURRENT_SKIN_MODE = "current_skin_mode";
+
+    private static final String NIGHT_SKIN_RESOURCE_NAME = "night_skin_resource.apk";
+
+    private Context mContext;
+    /**
+     * 皮肤资源对象
+     */
     private SkinResources mSkinResources;
+    private SharedPreferences mSharedPreferences;
 
     private static class SingletonHolder {
         private static final SkinConfigManager instance = new SkinConfigManager();
@@ -18,10 +42,46 @@ public class SkinConfigManager {
         return SingletonHolder.instance;
     }
 
-    public void init(Resources resources) {
-        mSkinResources = new SkinResources(resources);
+    public void init(Context context) {
+        mContext = context;
+        mSkinResources = new SkinResources(context.getResources());
+        mSharedPreferences = context.getSharedPreferences("SkinConfig", Context.MODE_PRIVATE);
         if (!EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().register(this);
         }
+        changeSkin();
+    }
+
+    public void changeSkin() {
+        if (isCurrentNightMode()) {
+            changeToNightMode();
+        } else {
+            changeToLightMode();
+        }
+    }
+
+    public boolean isCurrentNightMode() {
+        String currentSkinMode = SKIN_MODE_LIGHT;
+        if (mSharedPreferences != null) {
+            currentSkinMode = mSharedPreferences.getString(KEY_CURRENT_SKIN_MODE, SKIN_MODE_LIGHT);
+        }
+        return TextUtils.equals(currentSkinMode, SKIN_MODE_NIGHT);
+    }
+
+    private void changeToNightMode() {
+
+    }
+
+    private void changeToLightMode() {
+
+    }
+
+    private String getNightSkinPath() {
+        String skinDirName = mContext.getFilesDir().getAbsolutePath() + File.separator + "skin";
+        File file = new File(skinDirName);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return skinDirName + File.separator + NIGHT_SKIN_RESOURCE_NAME;
     }
 }
